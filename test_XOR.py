cb1d890ce01dcedc20c5eb2140e28bc51c9e0753
Joshua Ward <jward@illuminate.solutions>
2020-02-18 00:38:31 -0800

    add initial XOR fitness function test

--- test_XOR.py
+++ test_XOR.py
@@ -34,13 +34,51 @@ def configLogfile():
         filename=logFile, level=logging.INFO)
 
 
+def xor(solutionList):
+    '''
+    0 0 | 0 1 | 1 0 | 11
+     0   |   1  |   1  |   0
+
+    '''
+    if solutionList is [0, 0]:
+        return 0
+    elif solutionList is [0, 1]:
+        return 1
+    elif solutionList is [1, 0]:
+        return 0
+    elif solutionList is [1, 1]:
+        return 1
+    else:
+        raise Exception("wrong values sent to xor")
+
+
 def myFunc(genome):
+    # TODO: use python type enforcement since functional
+    # (not necessarily ctypes but cython is a goal unless jax/numpy is sufficient)
     '''
     example function for evaluation. always takes a genome and returns a float.
     '''
-    # TODO: use python type enforcement since functional
-    # (not necessarily ctypes but cython is a goal unless jax/numpy is sufficient)
-    return rand.uniform(0, 1)
+    numTries = 11
+    # needs to be random to prevent memorizing order of input
+    for _ in range(0, numTries):
+        entry1 = rand.randint(0, 1)
+        entry2 = rand.randint(0, 1)
+        score = 0
+
+        output = genome.forwardProp([entry1, entry2])[0]
+
+        if output > 0.5:
+            if entry1 == 1 or entry2 == 1:
+                # one
+                if not entry1 == 1 and entry2 == 1:
+                    score += 1
+        elif output <= 0.5 and entry1 == 1 and entry2 == 1:
+            score += 1
+        elif output <= 0.5 and entry1 == 0 and entry2 == 0:
+            score += 1
+
+    score = score/numTries
+    return score
 
 
 class TestGenepool(unittest.TestCase):
@@ -56,11 +94,11 @@ class TestGenepool(unittest.TestCase):
         # NOTE: this test if a genome is crossed over with itself the same genome is produced as offspring (diversity singularity)
 
         configLogfile()
-        evaluation = evaluator(inputs=2, outputs=2, population=5,
-                               connectionMutationRate=0.5, nodeMutationRate=0.2)
+        evaluation = evaluator(inputs=2, outputs=1, population=100,
+                               connectionMutationRate=0.8, nodeMutationRate=0.8, weightMutationRate=0.8)
 
         # evaluate 50 generations
-        for _ in range(0, 50):
+        for _ in range(0, 20):
             evaluation.nextGeneration(myFunc)
         for c in evaluation.genepool:
             graphvizNEAT(c, 'sample-genome' + str(uuid.uuid1()))
