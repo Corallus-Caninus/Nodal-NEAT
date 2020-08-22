4d18450bf1345aed1e39d1482ed6a34b6df2f66e
Josh <ward.joshua92@yahoo.com>
2020-08-22 00:38:24 -0700

    add versioning

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name='Nodal_NEAT', packages=find_packages(), install_requires=['graphviz', 'matplotlib'],
+setup(name='Nodal_NEAT',
+      packages=find_packages(),
+      install_requires=['graphviz', 'matplotlib'],
+      version='1.0.0'
       )
 # TODO: wheel in graphviz backend
